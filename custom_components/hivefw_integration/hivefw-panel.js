f11c53a6fe04f1ece77d19260c4db919b0f2d36c
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2026-09-21 18:05:16 +0100

    Unify HiveFW header logo rendering

--- custom_components/hivefw_integration/hivefw-panel.js
+++ custom_components/hivefw_integration/hivefw-panel.js
@@ -1344,13 +1344,11 @@ class HiveFWPanel extends BasePanel {
         width:152px;
         height:38px;
         flex:0 0 auto;
-        position:relative;
         background:
           transparent
-          url('/hivefw_integration_panel/hivefw-wordmark.png?v=dark-1')
+          url('/hivefw_integration_panel/hivefw-wordmark.png?v=dark-2')
           center/contain
           no-repeat !important;
-        filter:invert(1) hue-rotate(180deg);
         -webkit-mask:none !important;
         mask:none !important;
       }
@@ -1414,17 +1412,9 @@ class HiveFWPanel extends BasePanel {
         width:152px;
         height:38px;
         flex:0 0 auto;
-        position:relative;
-        overflow:visible;
-        background:transparent;
+        background:transparent url('/hivefw_integration_panel/hivefw-wordmark.png?v=dark-2') center/contain no-repeat;
       }
-      .hivefw-header-brand-white::before {
-        content:"";
-        position:absolute;
-        inset:0;
-        background:url('/hivefw_integration_panel/hivefw-wordmark.png?v=dark-1') center/contain no-repeat;
-        filter:invert(1) hue-rotate(180deg);
-      }
+      .hivefw-header-brand-white::before,
       .hivefw-header-brand-white::after {
         content:none;
       }
