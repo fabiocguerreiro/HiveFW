ed1c48f44e497fbe91b12c62146a9e5e9ed1ab1a
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2026-09-26 10:22:54 +0100

    Remove KISS protocol export from Android App

--- app/lib/protocol/protocol.dart
+++ app/lib/protocol/protocol.dart
@@ -1,7 +1,6 @@
 export 'commands.dart';
 export 'companion_decoder.dart';
 export 'companion_encoder.dart';
-export 'kiss.dart';
 export 'meshcore_uri.dart';
 export 'models.dart';
 export 'packet_hash.dart';
