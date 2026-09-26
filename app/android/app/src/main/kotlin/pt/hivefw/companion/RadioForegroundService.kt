ccc2ca259111416e59c2ee9ccc705430700abd7b
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2026-09-26 17:55:58 +0100

    Use one HiveFW icon for all Android notifications

--- app/android/app/src/main/kotlin/pt/hivefw/companion/RadioForegroundService.kt
+++ app/android/app/src/main/kotlin/pt/hivefw/companion/RadioForegroundService.kt
@@ -109,7 +109,7 @@ class RadioForegroundService : Service() {
         val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setContentTitle(getString(R.string.radio_notification_title, currentRadioName))
             .setContentText(contentText)
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setSmallIcon(R.drawable.ic_hivefw_notification)
             .setOngoing(true)
             .setCategory(NotificationCompat.CATEGORY_SERVICE)
 
